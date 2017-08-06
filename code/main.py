6a0bc25da74f1235299e8fdc2e2e4af3fa593ac4
Aran Carmon <aran.carmon@gmail.com>
2017-08-06 14:26:48 +0300

    Sample batch

--- code/main.py
+++ code/main.py
@@ -3,13 +3,13 @@ Created on Aug 2, 2017
 
 @author: alon
 '''
-from utils import *
 import numpy as np
 import tensorflow as tf
 
 
-
-if __name__ == '__main__':
+####### Testing functions #######
+def test_fileToDic():
+    from utils import fileToDic
     dic = fileToDic('../project_train_data.csv')
     print ("question:", dic[6750][0])
     print ("consistent:")
@@ -19,4 +19,13 @@ if __name__ == '__main__':
     print ("inconsistent:")
     for line in dic[6750][2]:
         print (line)
-    
+
+def test_generateBatch():
+    from rnn_model import Rnn_model
+    rnn = Rnn_model("../project_train_data.csv")
+    for entry in rnn.generateBatch(10):
+        print ("----------")
+        print (entry)
+
+if __name__ == '__main__':
+    test_generateBatch()
--- code/rnn_model.py
+++ code/rnn_model.py
@@ -4,15 +4,22 @@ Created on Aug 6, 2017
 @author: alon
 '''
 
+import random
+from utils import *
 
-
-class rnn_model:
-    def __init__(self, data, batchSize):
-        self.data = data
-        self.keys = []
-        self.batch_size
+class Rnn_model:
+    def __init__(self, fileName):
+        dic = fileToDic(fileName)
+        self.dic = dic
+        self.keys = self.dic.keys()
         
     def generateBatch(self, batchSize):
-        if len(self.keys) == 0:
-            self.keys  += self.data.keys()
-        
+        def sample_consistent(key):
+            return random.sample(self.dic[key][1], 1)[0]
+        def sample_inconsistent(key):
+            return random.sample(self.dic[key][2], 1)[0]
+
+        batch_keys = random.sample(self.keys, batchSize)
+        batch = [(key, sample_consistent(key), sample_inconsistent(key)) for key in batch_keys]
+
+        return batch

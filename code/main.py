160e43942713120b6bc71fb799f3333c255f7064
Aran Carmon <aran.carmon@gmail.com>
2017-08-06 13:23:34 +0300

    Testing code

--- code/main.py
+++ code/main.py
@@ -11,5 +11,12 @@ import tensorflow as tf
 
 if __name__ == '__main__':
     dic = fileToDic('../project_train_data.csv')
-    print (list(enumerate(dic[6750][2])))
+    print ("question:", dic[6750][0])
+    print ("consistent:")
+    for line in dic[6750][1]:
+        print (line)
+    print ("")
+    print ("inconsistent:")
+    for line in dic[6750][2]:
+        print (line)
     

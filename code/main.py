27fbe73234b93a21884803d08b23c13f15533e9e
Alon Resler <alonress@gmail.com>
2017-08-06 12:04:01 +0300

    first commit

---
+++ code/main.py
@@ -0,0 +1,15 @@
+'''
+Created on Aug 2, 2017
+
+@author: alon
+'''
+from code.utils import *
+import numpy as np
+import tensorflow as tf
+
+
+
+if __name__ == '__main__':
+    dic = fileToDic('../paragram-phrase-XXL.txt')
+    print ("hello world")
+    
---
+++ code/utils.py
@@ -0,0 +1,23 @@
+'''
+Created on Aug 2, 2017
+
+@author: alon
+'''
+def printHello():
+    print("Hello")
+
+
+
+def fileToDic(fileName, separator=" "):
+    """
+    take input file where each line is <key><separtor><value>
+    and return a dictionary
+    """
+    dic = {'a' : 3}
+#     with open(fileName) as f:
+#         for line in f:
+#             print (line)
+#             key, value = line.split()
+#             dic[key] = value    
+    return dic
+    

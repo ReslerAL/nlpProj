56c30fafd5a0cb79d155766e85368b305e5fa31c
Alon Resler <alonress@gmail.com>
2017-08-08 17:27:17 +0300

    few changes

--- code/rnn_model.py
+++ code/rnn_model.py
@@ -4,8 +4,24 @@ Created on Aug 6, 2017
 @author: alon
 '''
 
+from __future__ import print_function, division
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 import random
 from utils import *
+
+num_epochs = 100
+total_series_length = 50000
+truncated_backprop_length = 15
+state_size = 4
+num_classes = 2
+echo_step = 3
+batch_size = 5
+num_batches = total_series_length//batch_size//truncated_backprop_length
+
+embedding_dim = 300
+
 
 class Rnn_model:
     

14a9c6850884f418d4f2adae7b3f7f7b90d6186b
dormuhlg <dormuhlg@gmail.com>
2017-08-24 15:54:29 +0300

    fixing evluation

--- code/simple_evaluator.py
+++ code/simple_evaluator.py
@@ -9,37 +9,37 @@ import math
 
 from sklearn.metrics.pairwise import cosine_similarity
 
-#utils functions
-def remove_apostrophe(str):
-    return str.replace("'", "")
-
-def my_cosine_similarity(x, y):
-    return cosine_similarity(x.reshape(1, -1), y.reshape(1, -1))
-
 class Evaluator:
     
     def __init__(self, model, fileName):
         self.model = model
         self.dic = self.parseFile(fileName)
-        self.basic_normalized_eval_list = []
-        self.softmax_eval_list = []
-        self.elimination_eval_list = []
+
+    @staticmethod
+    def remove_apostrophe(s):
+        return s.replace("'", "")
+
+    @staticmethod
+    def cosine_sim(x, y):
+        return cosine_similarity(x.reshape(1, -1), y.reshape(1, -1))
+
 
     def parseFile(self, fileName):
         dic = {}
+        print ("read evaluation dataset: ", fileName)
         with open(fileName) as f:
             for line in f:
                 #Parse line
-                line = remove_apostrophe(line)
+                line = Evaluator.remove_apostrophe(line)
                 line = line.split('\t')
                 assert len(line) == 5
                 lid, question, canonical, logical, isCorrect = line
                 if lid == "":
                     break
                 lid = int(lid)
-                for sym in "-+.^:,?!'":
+                for sym in "-+.^:,?!'()\"":
                     question = question.replace(sym, "")
-                for sym in "-+^:,?!'":
+                for sym in "-+^:,?!'()\"":
                     canonical = canonical.replace(sym, "")
                 #Convert isCorrect to boolean
                 assert isCorrect in ("True\n", "False\n"), "isCorrect: <" + str(isCorrect) + ">"
@@ -55,16 +55,16 @@ class Evaluator:
                 else:
                     dic[lid][2] = list(set(dic[lid][2] + [canonical]))
                     dic[lid][4] = list(set(dic[lid][4] + [logical]))
+        print("done!")
         return dic
-    
 
     def process_similarities(self, qid):
         question_embedding = self.model.apply(self.dic[qid][0])
         correct_canonical_embeddings = [self.model.apply(canonical) for canonical in self.dic[qid][1]]
         spurious_canonical_embeddings = [self.model.apply(canonical) for canonical in self.dic[qid][2]]
-        correct_cosine_similarities = [my_cosine_similarity(question_embedding, canonical_embedding) for
+        correct_cosine_similarities = [Evaluator.cosine_sim(question_embedding, canonical_embedding) for
                                        canonical_embedding in correct_canonical_embeddings]
-        spurious_cosine_similarities = [my_cosine_similarity(question_embedding, canonical_embedding) for
+        spurious_cosine_similarities = [Evaluator.cosine_sim(question_embedding, canonical_embedding) for
                                          canonical_embedding in spurious_canonical_embeddings]
 
         sims = correct_cosine_similarities + spurious_cosine_similarities
@@ -75,147 +75,89 @@ class Evaluator:
             sims = (sims.tolist(),)
         return sims, labels
 
-    def sims_to_rewards(self, qid, sims, p=0.2, verbose=True):
+    @staticmethod
+    def sims_to_rewards_softmax(sims, p=0.2):
+        elim_rewards = Evaluator.sims_to_rewards_elimination(sims, p)
+        elim_sims = np.multiply(elim_rewards, sims)
+        elim_sims = [float("-inf") if x == 0 else x for x in elim_sims]
+        scaled = np.multiply(elim_sims, [10]*len(elim_sims))
+        e_vec = np.exp(scaled - np.max(scaled))
+        rewards = e_vec / e_vec.sum()
+        return rewards
+
+    @staticmethod
+    def sims_to_rewards_elimination(sims, p=0.2):
         rewards = [1]*len(sims)
         ranked = np.argsort(sims)
         threshold = int(p * len(sims))
         eliminated = ranked[:threshold]
         for i in eliminated:
             rewards[i] = 0
-
-        if (verbose):
-            j = 0
-            logical_forms = self.dic[qid][3] + self.dic[qid][4]
-            canons = self.dic[qid][1] + self.dic[qid][2]
-            question = self.dic[qid][1] + self.dic[qid][2]
-            for i in reversed(ranked):
-
-                if len(ranked) - j == threshold:
-                    print("---------------------------------------------------")
-
-                print("\t", str(j), "logical:", logical, "canonical:", canonical, "correct:", label, "similarity:",
-                      sims[i])
-                j = j + 1
-
         return rewards
 
-    def score_results(self, rewards, lables):
-        kept = [index for index, value in enumerate(rewards) if value == 1]
+    @staticmethod
+    def sims_to_rewards(sims, p=0.2, soft=True):
+        if soft:
+            return Evaluator.sims_to_rewards_softmax(sims, p)
+        else:
+            return Evaluator.sims_to_rewards_elimination(sims, p)
+
+    @staticmethod
+    def score_results(rewards, lables):
+        kept = [index for index, value in enumerate(rewards) if value > 0]
         eliminated = [index for index, value in enumerate(rewards) if value == 0]
         assert len(kept) > 0
-        kept_noise = 1 - (np.sum(lables[kept]) / len(kept))
-        if (len(eliminated) > 0):
+        rewards = np.array(rewards)
+        #print("DEBUG rewards: ", rewards)
+        correct_rewards = np.multiply(lables[kept], rewards[kept])
+        #print("DEBUG kept_lables: ", lables[kept])
+        #print("DEBUG kept_rewards: ", rewards[kept])
+        #print("DEBUG correct: ", correct_rewards)
+        kept_noise = 1 - (np.sum(correct_rewards) / np.sum(rewards))
+        if len(eliminated) > 0:
             eliminated_noise = np.sum(lables[eliminated]) / len(eliminated)
         else:
             eliminated_noise = 0
         return kept_noise, eliminated_noise
 
-    def eval(self, p=.2, verbose=True):
+    def eval(self, p=.2, soft=False, verbose=True):
         kept_noise_list = []
         eliminated_noise_list = []
+        correct_elim_cnt = 0
         for qid in self.dic.keys():
             sims, lables = self.process_similarities(qid)
-            rewards = self.sims_to_rewards(sims, p)
-            kept_noise, eliminated_noise = self.score_results(rewards, lables)
+            rewards = Evaluator.sims_to_rewards(sims, p, soft)
+            kept_noise, eliminated_noise = Evaluator.score_results(rewards, lables)
             kept_noise_list.append(kept_noise)
             eliminated_noise_list.append(eliminated_noise)
+            if kept_noise == 1:
+                correct_elim_cnt = correct_elim_cnt + 1
 
-        print("number of questions:", len(self.dic.keys()))
-        print("p: elimination proportion used:", p)
-        print("Mean kept noise:", np.mean(kept_noise_list))
-        print("Max kept noise:", np.max(kept_noise_list))
-        print("Min kept noise:", np.min(kept_noise_list))
-        print("Mean eliminated noise:", np.mean(eliminated_noise_list))
-
-
-    def eval2(self, p=.2, verbose=True):
-        #F_ratio is F+/F, i.e. ratio of correct items in the eliminated items
-        F_ratio_list = []
-        #S_ratio is S-/F, i.e. ratio of spurious items in the kept items
-        S_ratio_list = []
-        count_questions_that_lost_correct_canonical_forms = 0
-        count_questions_that_were_discarded = 0
-        count_total_number_of_questions = 0
-
-        for lid in self.dic.keys():
-            #Process cosine similarities for all canonicals
-            question_embedding = self.model.apply(self.dic[lid][0])
-            correct_canonical_embeddings = [self.model.apply(canonical) for canonical in self.dic[lid][1]]
-            incorrect_canonical_embeddings = [self.model.apply(canonical) for canonical in self.dic[lid][2]]
-            correct_cosine_similarities = [my_cosine_similarity(question_embedding, canonical_embedding) for canonical_embedding in correct_canonical_embeddings]
-            incorrect_cosine_similarities = [my_cosine_similarity(question_embedding, canonical_embedding) for canonical_embedding in incorrect_canonical_embeddings]
-            all_cosine_similarities = correct_cosine_similarities + incorrect_cosine_similarities
-            all_cosine_similarities = np.array(all_cosine_similarities)
-            labels = np.array(([1] * len(correct_cosine_similarities)) + ([0] * len(incorrect_cosine_similarities)))
-            all_cosine_similarities = np.squeeze(all_cosine_similarities)
-            if all_cosine_similarities.shape == ():
-                all_cosine_similarities = (all_cosine_similarities.tolist(),)
-
-            canonical_forms = self.dic[lid][1] + self.dic[lid][2]
-            logical_forms = self.dic[lid][3] + self.dic[lid][4]
-
-            count_total_number_of_questions += 1
-            c = len(correct_canonical_embeddings)
-            i = len(incorrect_canonical_embeddings)
-            n = c + i
-            #P is how many to keep
-            P = math.ceil(p * n)
-            
-            if (i < 1) or (c < 1):
-                count_questions_that_were_discarded += 1
-                #Next question
-                continue
-
-            ranked = np.argsort(all_cosine_similarities)
-            keep_zone = True
-            if verbose:
+            #print question stats
+            if (verbose):
+                j = 0
+                ranked = np.argsort(sims)
+                logical_forms = self.dic[qid][3] + self.dic[qid][4]
+                canons = self.dic[qid][1] + self.dic[qid][2]
+                threshold = int(p * len(ranked))
                 print("###################################################")
-                print(str(lid) + ".", self.dic[lid][0])
-            S_minus = 0
-            F_plus = 0
-            F = 0
-            S = 0
-            for j in range(len(ranked)):
-                current_canonical_index = ranked[n-j-1]
-                canonical = canonical_forms[current_canonical_index]
-                logical = logical_forms[current_canonical_index]
-                label = labels[current_canonical_index]
-                similarity = all_cosine_similarities[current_canonical_index]
-                if (j >= P) and keep_zone:
-                    keep_zone = False
-                    if verbose:
+                print(str(qid) + ".", self.dic[qid][0])
+                for i in reversed(ranked):
+                    logical = logical_forms[i]
+                    canonical = canons[i]
+                    label = lables[i]
+                    sim = sims[i]
+                    reward = rewards[i]
+                    if len(ranked) - j == threshold:
                         print("---------------------------------------------------")
-                if (not keep_zone) and label == 1:
-                    F_plus += 1
-                if keep_zone and label == 0:
-                    S_minus += 1
-                if keep_zone:
-                    S += 1
-                else:
-                    F += 1
-                if verbose:
-                    print ("\t", str(j), "logical:", logical, "canonical:", canonical, "correct:", label, "similarity:", similarity)
+                    print("\t", str(j), "correct:", label, "reward:", reward, "similarity:", sim, "canonical:", canonical,
+                          "logical:", logical)
+                    j = j + 1
+                print("")
 
-            if F_plus == 0:
-                F_ratio = 0
-            else:
-                count_questions_that_lost_correct_canonical_forms += 1
-                F_ratio = float(F_plus) / float(F)
-
-            if S_minus == 0:
-                S_ratio = 0
-            else:
-                S_ratio = float(S_minus) / float(S)
-
-            F_ratio_list.append(F_ratio)
-            S_ratio_list.append(S_ratio)
-
-        if verbose:
-            print("###################################################")
-            print("")
-        print("p proportion used:", p)
-        print("Mean S ratio:", np.mean(S_ratio_list))
-        print("Mean F ratio:", np.mean(F_ratio_list))
-        print("count_questions_that_lost_correct_canonical_forms:", count_questions_that_lost_correct_canonical_forms)
-        print("count_questions_that_were_discarded:", count_questions_that_were_discarded)
-        print("count_total_number_of_questions:", count_total_number_of_questions)
+        # print final results
+        print("number of questions: ", len(self.dic.keys()))
+        print("p: elimination proportion used: ", p)
+        print("Mean kept noise: ", np.mean(kept_noise_list))
+        print("Mean eliminated noise: ", np.mean(eliminated_noise_list))
+        print("Number of questions without correct: ", correct_elim_cnt)
--- code/simple_model.py
+++ code/simple_model.py
@@ -4,11 +4,13 @@ import utils
 
 class SimpleModel:
     def __init__(self, file_name):
+        print("loading model's parameters...")
         with open(file_name, encoding='latin-1') as fh:
             data = np.loadtxt(fh, dtype="object")
         #Create a list of embedings and a mapping from word to embeddings index
         self.words = {_[1]:_[0] for _ in enumerate(data[:,0])}
         self.embeddings = data[:,1:].copy().astype("float64")
+        print("done!")
 
     def apply(self, sent, sess=None):
         sum_embeddings = 0
@@ -36,10 +38,11 @@ if __name__ == '__main__':
     data_file = "../evaluation_data.tsv"
     model = SimpleModel("../paragram-phrase-XXL.txt")
     evaluator = simple_evaluator.Evaluator(model, data_file)
-    if True:
-        for p in (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9):
-            evaluator.eval(p)
-            print()
+    verbose = True
+    if verbose:
+        evaluator.eval(0.5, True, verbose)
     else:
-        evaluator.eval(0.2)
+        for p in (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9):
+            evaluator.eval(p, True, verbose)
+            print()
 

b37c3f57c4dc630300ca64c572e16c615c168684
Alon Resler <alonress@gmail.com>
2017-08-30 11:20:29 +0300

    data changes

--- code/train.py
+++ code/train.py
@@ -22,11 +22,11 @@ import shutil
 config = {
     'batch_size' : 100,
     'embedding_dim' : 300,
-    'data_file' : '/home/alon/workspace/nlp/project_train_data.csv',
+    'data_file' : './nlml_train_data.tsv',
     'delta' : 0.4,
     'learning_rate' : 0.0005,
     'num_epocs' : 10,
-    'data_size'  : 369488,
+    'data_size'  : 516711,
     'lambda_c' :0.001,
     'lambda_w' : 1e-06,
     'print_freq' : 100
--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # run example "./eval.sh 1503569471412 0.5 false" 
 WORD_EMBDS_FILE=paragram-phrase-XXL.txt
-EVAL_DATA_FILE=evaluation_data.tsv
+EVAL_DATA_FILE=eval_data_nlmlproj.tsv
 MODEL_DIR=./saved/$1/
 echo "Simple model evaluation:\n"
 python3 code/evaluate.py -model simple -embdsfile $WORD_EMBDS_FILE -evaldata $EVAL_DATA_FILE -elimination $2 -verbose $3

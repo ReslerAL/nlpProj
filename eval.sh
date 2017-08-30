af646527ad4195dcb24f484d4a74f956126fe2a6
ReslerAL <alonress@gmail.com>
2017-08-30 16:18:56 +0300

    add softmax arg

--- eval.sh
+++ eval.sh
@@ -5,6 +5,6 @@ WORD_EMBDS_FILE=paragram-phrase-XXL.txt
 EVAL_DATA_FILE=eval_data_nlmlproj.tsv
 MODEL_DIR=./saved/$1/
 echo "Simple model evaluation:\n"
-python3 code/evaluate.py -model simple -embdsfile $WORD_EMBDS_FILE -evaldata $EVAL_DATA_FILE -elimination $2 -verbose $3
+python3 code/evaluate.py -model simple -embdsfile $WORD_EMBDS_FILE -evaldata $EVAL_DATA_FILE -elimination $2 -softmax $3
 echo "\nLstm model ($1) evaluation:\n"
-python3 code/evaluate.py -model lstm -dir $MODEL_DIR -evaldata $EVAL_DATA_FILE -elimination $2 -verbose $3
+python3 code/evaluate.py -model lstm -dir $MODEL_DIR -evaldata $EVAL_DATA_FILE -elimination $2 -softmax $3

5af5f14e955e57ffc4eb4938806f02eaf919cab5
Alon Resler <alonress@gmail.com>
2017-08-30 11:49:28 +0300

    changing data file

--- eval_all.sh
+++ eval_all.sh
@@ -3,5 +3,5 @@ models=$(ls saved/)
 for model in $models
 do
 	echo "evaluting model $model...\n" 
-	python3 code/evaluate.py -model lstm -dir ./saved/${model}/ -evaldata evaluation_data.tsv -elimination $1 -verbose false
+	python3 code/evaluate.py -model lstm -dir ./saved/${model}/ -evaldata eval_data_nlmlproj.tsv -elimination $1 -verbose false
 done

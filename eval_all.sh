bfdb60fe0ce86e827050eba81fda9299ceb852f5
ReslerAL <alonress@gmail.com>
2017-08-30 19:19:51 +0300

    edit script

--- eval_all.sh
+++ eval_all.sh
@@ -3,6 +3,7 @@ export CUDA_VISIBLE_DEVICES=0
 models=$(ls saved/) 
 for model in $models
 do
-	echo "evaluting model $model...\n" 
+	echo "evaluting model $model" 
 	python3 code/evaluate.py -model lstm -dir ./saved/${model}/ -evaldata eval_data_nlmlproj.tsv -elimination $1 -softmax $2
+	echo '\n'
 done

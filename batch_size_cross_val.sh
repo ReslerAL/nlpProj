0517a361c535c85d07cd12891648b9ab7bc3286b
Alon Resler <alonress@gmail.com>
2017-08-31 14:42:18 +0300

    edit script

--- batch_size_cross_val.sh
+++ batch_size_cross_val.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-export CUDA_VISIBLE_DEVICES=$1
+#export CUDA_VISIBLE_DEVICES=$1
 for b in 25 50 100 200
 do
   echo "Train with batch_size $b"
-  python3 code/train.py -batch_size $b
+  CUDA_VISIBLE_DEVICES=$1 python3 code/train.py -batch_size $b
 done
 

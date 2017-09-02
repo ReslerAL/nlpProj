97f51fe295087625d823be21c7a6b07dfcaef725
Alon Resler <alonress@gmail.com>
2017-09-02 23:39:44 +0300

    add

--- p_cross_val.sh
+++ p_cross_val.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for p in 0.9995 0.9999 0.99995 0.99999
+for p in 0.9995 0.9999 0.99995 0.99999 0.999999
 do
   echo "Train with p = $p"
   CUDA_VISIBLE_DEVICES=$1 python3 code/train.py -p $p
